6b40b371db5065901ca7979e5be1e5289a1fbe6b
Tobias Schmidt <t.schmidt@rubidat.de>
2017-09-26 16:07:04 +0200

    image_url migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629225503) do
+ActiveRecord::Schema.define(version: 20170926130859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -217,6 +217,7 @@ ActiveRecord::Schema.define(version: 20170629225503) do
     t.boolean  "app_commentator"
     t.boolean  "app_creator"
     t.boolean  "app_publisher"
+    t.string   "image_thumb_url"
   end
 
   add_foreign_key "creatorships", "creators"
